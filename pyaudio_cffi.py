c139234f7de012057f9bd98277028e01654889f5
Bastian Bechtold <bb@Bastians-MacBook-Pro.local>
2013-07-29 16:29:23 +0200

    implement reading and fix writing

--- pyaudio_cffi.py
+++ pyaudio_cffi.py
@@ -126,10 +126,28 @@ void Pa_Sleep (long);
 
 float32 = 0x00000001
 int32   = 0x00000002
-int24   = 0x00000004
+# int24   = 0x00000004 Not supported by numpy!
 int16   = 0x00000008
 int8    = 0x00000010
 uint8   = 0x00000020
+
+_pa2np = {
+    float32: np.float32,
+    int32: np.int32,
+    # int24: None, Not supported by numpy!
+    int16: np.int16,
+    int8: np.int8,
+    uint8: np.uint8
+}
+
+_npsizeof = {
+    np.float32: 4,
+    np.int32: 4,
+    # np.int24: 3, Not supported by numpy!
+    np.int16: 2,
+    np.int8: 1,
+    np.uint8: 1
+}
 
 _pa = ffi.dlopen('portaudio')
 _pa.Pa_Initialize()
@@ -192,13 +210,13 @@ class Stream(object):
     def __init__(self, sample_rate=44100, block_length=1024,
                  input_device=None, output_device=None,
                  callback=None, finished_callback=None,
-                 flags={}):
+                 **flags):
         if input_device is None:
             input_device = default_input_device()
         if output_device is None:
             output_device = default_output_device()
         if callback is not None or finished_callback is not None:
-            raise NotImplementedError("stream callbacks are noct implemented yet")
+            raise NotImplementedError("stream callbacks are not implemented yet")
 
         input_stream_parameters = ffi.new("PaStreamParameters*",
                                           { 'device': input_device['device_index'],
@@ -206,6 +224,8 @@ class Stream(object):
                                             'sampleFormat': input_device['sample_format'],
                                             'suggestedLatency': input_device['input_latency'],
                                             'hostApiSpecificStreamInfo': ffi.NULL })
+        self._input_format = _pa2np[input_stream_parameters.sampleFormat]
+        self._input_channels = input_stream_parameters.channelCount
         if not input_device['interleaved_data']:
             input_stream_parameters.sampleFormat |= 0x80000000
 
@@ -214,7 +234,9 @@ class Stream(object):
                                             'channelCount': output_device['output_channels'],
                                             'sampleFormat': output_device['sample_format'],
                                             'suggestedLatency': output_device['output_latency'],
-                                            'hostApiSpecificStreamInfo': ffi.NULL })
+                                             'hostApiSpecificStreamInfo': ffi.NULL })
+        self._output_format = _pa2np[output_stream_parameters.sampleFormat]
+        self._output_channels = output_stream_parameters.channelCount
         if not output_device['interleaved_data']:
             output_stream_parameters.sampleFormat |= 0x80000000
 
@@ -223,7 +245,7 @@ class Stream(object):
             stream_flags |= 0x00000001
         if 'no_dithering' in flags:
             stream_flags |= 0x00000002
-        if 'never_drop_input' in flags:
+        if 'never_drop_input' in flags and flags['never_drop_input']:
             stream_flags |= 0x00000004
         if 'prime_output_buffers_using_callback' in flags:
             stream_flags |= 0x00000008
@@ -236,10 +258,14 @@ class Stream(object):
 
     def _handle_error(self, err):
         if err >= 0: return err
-        raise IOError(ffi.string(_pa.Pa_GetErrorText(err)))
+        print("Warning (%.4f): %s" % (self.time(), ffi.string(_pa.Pa_GetErrorText(err))))
 
     def __del__(self):
-        self._handle_error(_pa.Pa_CloseStream(self._stream[0]))
+        # At program shutdown, _pa is sometimes deleted before this
+        # function is called. However, in that case, Pa_Terminate
+        # already took care of closing all dangling streams.
+        if _pa:
+            self._handle_error(_pa.Pa_CloseStream(self._stream[0]))
 
     def start(self):
         self._handle_error(_pa.Pa_StartStream(self._stream[0]))
@@ -275,24 +301,37 @@ class Stream(object):
     def cpu_load(self):
         return _pa.Pa_GetStreamCpuLoad(self._stream[0])
 
-    def read(self):
-        raise NotImplementedError("stream.read is not implemented yet")
-
-    def write(self, data):
-        if isinstance(data, np.ndarray):
-            _data = ffi.cast("float *", data.flatten().ctypes.data)
-            self._handle_error(_pa.Pa_WriteStream(self._stream[0], _data, len(data)))
+    def read(self, num_frames=1024, raw=False):
+        num_bytes = self._input_channels*_npsizeof[self._input_format]*num_frames
+        data = ffi.new("char[]", num_bytes)
+        self._handle_error(_pa.Pa_ReadStream(self._stream[0], data, num_frames))
+        if raw:
+            return data
         else:
-            self._handle_error(_pa.Pa_WriteStream(self._stream[0], data, len(data)))
+            data = np.fromstring(ffi.buffer(data), dtype=self._input_format,
+                                 count=num_frames*self._input_channels)
+            return np.reshape(data, (num_frames, self._input_channels))
+
+    def write(self, data, num_frames=None):
+        num_frames = num_frames or len(data)
+        if isinstance(data, np.ndarray):
+            if data.dtype != self._output_format:
+                data = np.array(data, dtype=self._output_format)
+            data = data.flatten().tostring()
+        elif isinstance(data, list):
+            data = np.array(data, dtype=self._output_format).flatten().tostring()
+        self._handle_error(_pa.Pa_WriteStream(self._stream[0], data, num_frames))
 
 if __name__ == '__main__':
     from scipy.io.wavfile import read as wavread
     fs, wave = wavread('thistle.wav')
     wave = np.array(wave, dtype=np.float32)
     wave /= 2**15
-    block_length = 128
+    block_length = 1
     s = Stream(sample_rate=fs, block_length=block_length)
     s.start()
+    for n in range(int(fs*5/block_length)):
+        s.write(s.read(block_length))
     for idx in range(0, wave.size, block_length):
         s.write(wave[idx:idx+block_length])
     s.stop()
