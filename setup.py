262a3cdac4b97259b53cf10b3fab8f898514606e
Bastian Bechtold <basti@bastibe.de>
2013-08-14 12:40:30 +0200

    added simple setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyAudio-CFFI',
+      version='0.1',
+      description='An audio library based on PortAudio, CFFI and NumPy',
+      author='Bastian Bechtold',
+      author_email='basti@bastibe.de',
+      url='https://github.com/bastibe/PyAudio-CFFI',
+      py_modules=['pyaudio_cffi'])
