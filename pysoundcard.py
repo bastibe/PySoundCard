90f1f4727471558eb0b5f2d7905750345c026986
Matthias Geier <Matthias.Geier@gmail.com>
2014-06-17 21:13:48 +0200

    Remove PaHostApiTypeId from cdef
    The enum values are never used.
    Also, for the use in Python, the host API names should be sufficient.

--- pysoundcard.py
+++ pysoundcard.py
@@ -105,26 +105,9 @@ const char *Pa_GetVersionText(void);
 
 typedef int PaDeviceIndex;
 
-typedef enum PaHostApiTypeId {
-    paInDevelopment=0,
-    paDirectSound=1,
-    paMME=2,
-    paASIO=3,
-    paSoundManager=4,
-    paCoreAudio=5,
-    paOSS=7,
-    paALSA=8,
-    paAL=9,
-    paBeOS=10,
-    paWDMKS=11,
-    paJACK=12,
-    paWASAPI=13,
-    paAudioScienceHPI=14
-} PaHostApiTypeId;
-
 typedef struct PaHostApiInfo {
     int structVersion;
-    PaHostApiTypeId type;
+    enum PaHostApiTypeId type;
     const char *name;
     int deviceCount;
     PaDeviceIndex defaultInputDevice;
